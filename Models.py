912aea49d6d81aeac2550a4049485070373c21e7
leletaurino <leletaurino@gmail.com>
2022-01-10 14:36:12 +0100

    - commit from macbook

--- Models.py
+++ Models.py
@@ -1,14 +1,30 @@
 from datetime import datetime, timedelta
-# from Server import db
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'fwefew32323432wfwgferge!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootadmin@localhost/testdb'
+
+# manager = Manager(app)
+db = SQLAlchemy(app)
 
 
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.Integer(), primary_key=True)
-#     username = db.Column(db.String(45), nullable=False, unique=True)
-#     password = db.Column(db.String(255), nullable=False)
-#     created_on = db.Column(db.DateTime(), default=datetime.now().date(), nullable=False)
-#     expiration_on = db.Column(db.DateTime(), default=datetime.now().date() + timedelta(days=30), nullable=False)
-#
-#     def __repr__(self):
-#         return "<{}:{}>".format(self.id, self.username)
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(45), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.now().date(), nullable=False)
+    expiration_on = db.Column(db.DateTime(), default=datetime.now().date() + timedelta(days=30), nullable=False)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.username)
+
+
+db.create_all()
+db.session.commit()
+
+CORS(app)
--- Server.py
+++ Server.py
@@ -1,36 +1,8 @@
-from flask import Flask, request, render_template, redirect, make_response, jsonify
+from flask import request, redirect, make_response
 from markupsafe import escape
 from flask import url_for
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
-from datetime import datetime, timedelta
-
-app = Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = 'fwefew32323432wfwgferge!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootadmin@localhost/testdb'
-
-# manager = Manager(app)
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(45), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
-    created_on = db.Column(db.DateTime(), default=datetime.now().date(), nullable=False)
-    expiration_on = db.Column(db.DateTime(), default=datetime.now().date() + timedelta(days=30), nullable=False)
-
-    def __repr__(self):
-        return "<{}:{}>".format(self.id, self.username)
-
-
-db.create_all()
-db.session.commit()
-
-CORS(app)
+from Models import app
 
 
 @app.errorhandler(404)
